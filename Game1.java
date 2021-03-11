6087bc7aba33a9aa0b2f2bd11ce02e1c6a78e20d
partha-cloud <partha11dey@gmail.com>
2021-03-11 16:58:52 +0530

    diceroll

--- Game1.java
+++ Game1.java
@@ -3,9 +3,8 @@ package Player;
 public class Game1 {
 
 	public static void main(String[] args) {
-		int initialPos=0;
-		System.out.println("Players initial position is: "+ initialPos);
-
+		int diceroll=1+(int)((Math.random() *10) %6);
+		System.out.println("dice roll: "+ diceroll);
 	}
 
 }
