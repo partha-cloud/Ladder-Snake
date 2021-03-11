a5c663a5ef22829aece0c9188df267a08fbd5c46
partha-cloud <partha11dey@gmail.com>
2021-03-11 17:27:05 +0530

    opton check

--- Game1.java
+++ Game1.java
@@ -3,8 +3,28 @@ package Player;
 public class Game1 {
 
 	public static void main(String[] args) {
-		int diceroll=1+(int)((Math.random() *10) %6);
-		System.out.println("dice roll: "+ diceroll);
+		final int NO_PLAY=0;
+		final int LADDER=1;
+		final int SNAKE=2;
+		int diceRoll = 1 + (int)( (Math.random() * 10) % 6);
+		System.out.println("The Dice Rolled to: " + diceRoll);
+		
+		int option = (int) ( (Math.random() * 10) % 3 );
+		switch(option) {
+			case NO_PLAY:
+				System.out.println("The option was to no play, hence the player will not advance");
+				break;
+			case LADDER:
+				System.out.println("The option came to Ladder, the player move forward: " + diceRoll + " steps");
+				break;
+			case SNAKE:
+				System.out.println("The option came to Snake, the player will backward: " + diceRoll + " steps");
+				break;
+			default:
+				System.out.println("Some error occured, default case");
+				break;
+		}
+
 	}
 
 }
